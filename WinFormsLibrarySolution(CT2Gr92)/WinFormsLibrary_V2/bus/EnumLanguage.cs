d85c8ea6acc921973a2998441d8cc065cdec0501
lennin <lenninssp1021@gmail.com>
2024-03-07 16:31:23 -0500

    al

---
+++ WinFormsLibrarySolution(CT2Gr92)/WinFormsLibrary_V2/bus/EnumLanguage.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace WinFormsLibrary_V2.bus
+{
+    public enum EnumLanguage
+    {
+        ENGLISH,
+        FRENCH,
+        SPANISH,
+        UNDEFINED
+    }
+}
